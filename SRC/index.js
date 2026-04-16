07a4ce788c886c0ef2752fe680c35ba1bfa52639
harrellnichcol <harrellnichcol@gmail.com>
2026-04-15 21:47:25 -0600

    updated search function

--- SRC/index.js
+++ SRC/index.js
@@ -1,8 +1,22 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "3ed75b5a3c03atf2beaa90eb081934of";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiUrl};
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
