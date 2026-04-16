f78907b7584ee4bda18dc03c7e71c9b7f9c1da86
harrellnichcol <harrellnichcol@gmail.com>
2026-04-15 22:19:32 -0600

    updated search function

--- SRC/index.js
+++ SRC/index.js
@@ -1,21 +1,22 @@
 function refreshWeather(response) {
-  let temperatureElement = document.querySelector("#temperature");
+  let temperatureElement = document.querySelector("#temperature"); // Added # for ID
   let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
-  console.log(response.data.temperature.current);
 }
 
 function searchCity(city) {
   let apiKey = "3ed75b5a3c03atf2beaa90eb081934of";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiUrl};
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`; // Fixed variable name
   axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
